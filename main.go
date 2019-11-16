540fc687a9e657ebc2f90012d39a61e292b70c0b
Bogdan Yanov <b.yanov76@gmail.com>
2019-11-16 22:16:01 +0200

    Little changes in main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var inputNum uint64 = 702
-	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotation(inputNum))
-	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotation2(inputNum))
-	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotationFor(inputNum))
+	fmt.Printf("Input num -> %d; Result -> %s\n", inputNum, components.GetSpreadsheetNotation(inputNum))
+	fmt.Printf("Input num -> %d; Result -> %s\n", inputNum, components.GetSpreadsheetNotation2(inputNum))
+	fmt.Printf("Input num -> %d; Result -> %s\n", inputNum, components.GetSpreadsheetNotationFor(inputNum))
 }
