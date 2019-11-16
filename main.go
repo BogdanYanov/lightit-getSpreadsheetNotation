477b20ed0026262e59fb3d8c69509c29119278ad
Bogdan Yanov <b.yanov76@gmail.com>
2019-11-16 22:14:51 +0200

    Little changes in main.go

--- main.go
+++ main.go
@@ -3,32 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/BogdanYanov/lightit-getSpreadsheetNotation/components"
-	"time"
 )
 
 func main() {
-	var (
-		start   time.Time
-		expired time.Duration
-	)
-
-	start = time.Now()
-
-	for i := 1; i < 10000; i++ {
-		components.GetSpreadsheetNotation(uint64(i))
-	}
-
-	expired = time.Since(start)
-
-	fmt.Printf("GetSpreadsheetNotation took %s\n", expired)
-
-	start = time.Now()
-
-	for i := 1; i < 10000; i++ {
-		components.GetSpreadsheetNotation2(uint64(i))
-	}
-
-	expired = time.Since(start)
-
-	fmt.Printf("GetSpreadsheetNotation2 took %s\n", expired)
+	var inputNum uint64 = 702
+	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotation(inputNum))
+	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotation2(inputNum))
+	fmt.Printf("Input num -> %d; Result -> %s", inputNum, components.GetSpreadsheetNotationFor(inputNum))
 }
